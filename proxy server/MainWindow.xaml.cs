11f6a497be6f93139a7b8ab6425e3fd3c0b07fef
thestarweb <597577939@qq.com>
2017-06-25 19:05:26 +0800

    弹幕代理服务器计划（未完工）第一步

---
+++ proxy server/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace proxy_server
+{
+	/// <summary>
+	/// MainWindow.xaml 的交互逻辑
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private bool isstart = false;
+		private Socket _socket = null;
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+		public void write(string info) {
+			log.Dispatcher.Invoke(
+					new Action(
+						 delegate
+						 {
+
+							 log.AppendText(info+"\n");
+						 }
+				   )
+			  );
+		}
+
+		private void start(object sender, RoutedEventArgs e)
+		{
+			if (isstart)
+			{
+				//
+			}
+			try
+			{
+				int p = int.Parse(port.Text);
+				_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+				_socket.Bind(new IPEndPoint(IPAddress.Any, p));
+				_socket.Listen(16);
+				_socket.BeginAccept(new AsyncCallback(onconnect), _socket);
+			}
+			catch (FormatException)
+			{
+				log.AppendText("端口号必须是整数\n");
+				return;
+			}
+			log.AppendText("开始监听\n");
+			isstart = true;
+		}
+		public void onconnect(IAsyncResult ar)
+		{
+			if(ar==null) {
+				return;
+			}
+			Socket s=_socket.EndAccept(ar);
+			s.Send(Encoding.ASCII.GetBytes("hello"));
+			connection c = new connection(s,this);
+			write("新连接:"+c.ip+":"+ c.port);
+			_socket.BeginAccept(new AsyncCallback(onconnect), _socket);
+
+		}
+		public void onclose(connection c) {
+			write("连接断开" + c.ip + ":" + c.port);
+		}
+	}
+}
---
+++ proxy server/connection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace proxy_server
+{
+	public class connection
+	{
+		private MainWindow win;
+		private Socket socket;
+		private Byte[] buff= new Byte[2048];
+		private string _ip;
+		public string ip { get { return _ip; } }
+		private int _port;
+		public int port { get { return _port; } }
+		public connection(Socket s,MainWindow w) {
+			socket = s;
+			win = w;
+			_ip = ((IPEndPoint)s.RemoteEndPoint).Address.ToString();
+			_port = ((IPEndPoint)s.RemoteEndPoint).Port;
+			s.BeginReceive(buff, 0, buff.Length, 0, new AsyncCallback(onread), s);
+		}
+		public void onread(IAsyncResult ar) {
+			int l= socket.EndReceive(ar);
+			if (l < 1) {
+				win.onclose(this);
+				socket.Close();
+				return;
+			}
+            win.write(ip + ":" + port+"："+Encoding.UTF8.GetString(buff, 0, l));
+			socket.BeginReceive(buff, 0, buff.Length, 0, new AsyncCallback(onread), socket);
+		}
+	}
+}
