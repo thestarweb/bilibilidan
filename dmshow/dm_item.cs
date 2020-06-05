301579a5e6696011e29f06d0fdb844dd5eb174b1
thestarweb <597577939@qq.com>
2020-06-05 22:04:46 +0800

    自己绘制弹幕提升效率（大概 至少视觉上好点）

---
+++ dmshow/dm_item.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace dmshow
+{
+    class dm_item
+    {
+        private static int  _maxWidth=200;
+        public static int maxWidth
+        {
+            get
+            {
+                return _maxWidth;
+            }
+            set
+            {
+                _maxWidth = value;
+            }
+        }
+        private static int _size = 12;
+        public static int size
+        {
+            get
+            {
+                return _size;
+            }
+            set
+            {
+                _size = value;
+            }
+        }
+        public static LinkedList<dm_item> list = new LinkedList<dm_item>();
+        public static void add(String sender, String data, int type)
+        {
+            list.AddFirst(new dm_item(sender, data, type));
+            cleanOutOfDate();
+        }
+        public static TimeSpan ts = new TimeSpan(0, 0, -10);
+        public static int cleanOutOfDate()
+        {
+            DateTime outTime = DateTime.Now.Add(ts);
+            int num = 0;
+            while (list.Count>0&&DateTime.Compare(list.Last.Value.time, outTime) < 0)
+            {
+                list.RemoveLast();
+                num++;
+            }
+            return num;
+        }
+        private FormattedText _data;
+        public FormattedText data
+        {
+            get
+            {
+                return _data;
+            }
+        }
+        private DateTime time;
+        private dm_item(String sender,String data,int type)
+        {
+            time = DateTime.Now;
+            _data =new FormattedText(sender+": "+data, new System.Globalization.CultureInfo("zh-CN", false), FlowDirection.LeftToRight, new Typeface("微软雅黑"),_size, Brushes.White);
+            _data.MaxTextWidth = maxWidth;
+            Brush b;
+            switch (type)
+            {
+                case 0:
+                    b = Brushes.SkyBlue;
+                    break;
+                case 1:
+                    b = Brushes.Red;
+                    break;
+                case 2:
+                    b = Brushes.Green;
+                    break;
+                case 3:
+                    b = Brushes.DarkRed;
+                    break;
+                case 11:
+                    b = Brushes.LightBlue;
+                    break;
+                case 12:
+                    b = Brushes.Orange;
+                    break;
+                case 13:
+                    b = Brushes.GreenYellow;
+                    break;
+                default:
+                    b = Brushes.White;
+                    break;
+
+            }
+            _data.SetForegroundBrush(b, 0, sender.Length);
+        }
+    }
+}
--- dmshow/show_list.xaml.cs
+++ dmshow/show_list.xaml.cs
@@ -22,87 +22,59 @@ namespace dmshow
     {
         public double x;
         public double y;
+        Thread checker;
         public show_list()
         {
+            initBackgroundBrush();
             InitializeComponent();
             y = 768 - 60;
             x = 1280 - this.Width - 10;
             this.Show();
-            dms.Document.Blocks.Remove(dms.Document.Blocks.FirstBlock);
+            this.Height = 500;
+            //dms.Document.Blocks.Remove(dms.Document.Blocks.FirstBlock);
+            this.set_weizhi(null);
+            checker = new Thread(remove_dm);
+            checker.IsBackground = true;
+            checker.Start();
         }
         public void adddm(int type, string name, string text)
         {
-            //dms.CaretBrush= Brush.
-            //dms.Document.Blocks.Remove(dms.Document.Blocks.FirstBlock);
-
-            dms.Dispatcher.Invoke(
-                    new Action(
-                         delegate
-                         {
-                             var p = new Paragraph();
-                             var _name = new Run(name);
-                             switch (type)
-                             {
-                                 case 0:
-                                     _name.Foreground = Brushes.SkyBlue;
-                                     break;
-                                 case 1:
-                                     _name.Foreground = Brushes.Red;
-                                     break;
-                                 case 2:
-                                     _name.Foreground = Brushes.Green;
-                                     break;
-                                 case 3:
-                                     _name.Foreground = Brushes.DarkRed;
-                                     break;
-                                 case 11:
-                                     _name.Foreground = Brushes.LightBlue;
-                                     break;
-                                 case 12:
-                                     _name.Foreground = Brushes.Orange;
-                                     break;
-                                 case 13:
-                                     _name.Foreground = Brushes.GreenYellow;
-                                     break;
-                                 default:
-                                     _name.Foreground = Brushes.White;
-                                     break;
-
-                             }
-                             var _text = new Run(" : " + text);
-                             _text.Foreground = Brushes.White;
-                             p.Inlines.Add(_name);
-                             p.Inlines.Add(_text);
-                             dms.Document.Blocks.Add(p);
-                             ThreadPool.QueueUserWorkItem(set_weizhi);
-                             ThreadPool.QueueUserWorkItem(remove_dm, p);
-                         }
-                   )
-              );
+            dm_item.add(name, text, type);
+            askReRender();
         }
         private void remove_dm(object o)
         {
-            Thread.Sleep(9000);
-
-            dms.Dispatcher.Invoke(
-                    new Action(
-                         delegate
-                         {
-                             dms.Document.Blocks.Remove(dms.Document.Blocks.FirstBlock);
-                             ThreadPool.QueueUserWorkItem(set_weizhi);
-                         }
-                   )
-              );
+            while (true)
+            {
+                Thread.Sleep(100);
+                if (dm_item.cleanOutOfDate() > 0)
+                {
+                    this.askReRender();
+                }
+            }
+        }
+        private void askReRender()
+        {
+            this.Dispatcher.Invoke(
+                new Action(
+                        delegate
+                        {
+                            InvalidateVisual();
+                        }
+                )
+            );
         }
         private void set_weizhi(object o)
         {
-            Thread.Sleep(1);
-            dms.Dispatcher.Invoke(
+            this.Dispatcher.Invoke(
                     new Action(
                          delegate
                          {
-                             this.Top = y - this.Height;
+                             this.Top = 0;
                              this.Left = x;
+                             this.Height = y;
+                             this.main.Height = y;
+                             //this.mai
                          }
                    )
               );
@@ -117,8 +89,31 @@ namespace dmshow
             //0x08000000=WS_EX_NOACTIVATE 窗口不会变成前台
             //0x80000=WS_EX_LAYERED 窗口具有透眀属性 大概是没用
             //0x20=WS_EX_TRANSPARENT 窗口透明 这里其实是指的窗口不会捕获鼠标事件 鼠标可以穿过窗口点击到后面的东西
-            Win32API.SetWindowLong(handle, -20, (IntPtr)(oldStyle | 0x00000080 | 0x08000000| 0x80000| 0x20));
+            //https://www.cnblogs.com/wangjixianyun/p/3427453.html
+            Win32API.SetWindowLong(handle, -20, (IntPtr)(oldStyle | 0x00000080 | 0x08000000 | 0x80000 | 0x20));
         }
-
+        private Pen groundPen = new Pen();
+        private Brush groundBrush;
+        public void initBackgroundBrush()
+        {
+            BrushConverter brushConverter = new BrushConverter();
+            groundBrush = (Brush)brushConverter.ConvertFromString("#80000000");
+        }
+        protected override void OnRender(DrawingContext dc)
+        {
+            double baseH = y;
+            foreach (dm_item item in dm_item.list)
+            {
+                baseH -= item.data.Height;
+                Rect r = new Rect();
+                r.X = 0;
+                r.Y = baseH;
+                r.Width = dm_item.maxWidth+10;
+                r.Height = item.data.Height;
+                dc.DrawRectangle(groundBrush, groundPen, r);
+                Point p = new Point(5, baseH);
+                dc.DrawText(item.data, p);
+            }
+        }
     }
 }
